d0510a86a0a2d6ab349103638d938d21e1a7b44c
Adenly <37798429+Adenly@users.noreply.github.com>
2020-08-09 01:12:48 +0200

    Add files via upload

---
+++ upvoting/App/seed.js
@@ -0,0 +1,42 @@
+window.Seed = (function () {
+  const submissions = [
+    {
+      id: 1,
+      title: 'Sand on Sale',
+      description: 'On-demand sand castle construction expertise.',
+      url: '#',
+      votes: 16,
+      avatar: '../public/images/avatars/danny.png',
+      submissionImage: '../public/images/submissions/sand.jpg',
+    },
+    {
+      id: 2,
+      title: 'The Fantasy League',
+      description: 'Earn points when you pass legislation.',
+      url: '#',
+      votes: 11,
+      avatar: '../public/images/avatars/kris.png',
+      submissionImage: '../public/images/submissions/League.jpg',
+    },
+    {
+      id: 3,
+      title: 'Hats on!',
+      description: 'We have your measurements and shipping address ready.',
+      url: '#',
+      votes: 17,
+      avatar: '../public/images/avatars/vero.jpg',
+      submissionImage: '../public/images/submissions/hat.jpg',
+    },
+    {
+      id: 4,
+      title: 'Haught or Naught',
+      description: 'High-minded or absent-minded? You decide.',
+      url: '#',
+      votes: 9,
+      avatar: '../public/images/avatars/molly.jpg',
+      submissionImage: '../public/images/submissions/mind.gif',
+    }
+  ];
+
+  return { submissions: submissions };
+}());
